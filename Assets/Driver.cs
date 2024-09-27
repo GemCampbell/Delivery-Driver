eebef2c7049d777ad23965965eaad35d1d2d0ca4
Gemma Campbell <gem.cam89@outlook.com>
2024-09-27 17:23:28 +0100

    Added: Rotation, Translation, Steer/Move variables, Time.deltaTime

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] float steerSpeed = 1f;
+    [SerializeField] float moveSpeed = 0.01f;
+    
     void Start()
     {
         
@@ -11,6 +14,10 @@ public class Driver : MonoBehaviour
 
     void Update()
     {
-        
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
