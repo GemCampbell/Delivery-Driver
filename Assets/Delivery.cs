0f2a1f2b92346a8057d4caea45372ddbe65e75b9
Gemma Campbell <gem.cam89@outlook.com>
2024-10-02 18:30:13 +0100

    Added: If statements, Tags, Bools, Destroy()

---
+++ Assets/Delivery.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Delivery : MonoBehaviour
+{
+    bool hasPackage;
+    [SerializeField] float destroyDelay = 1f;
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        Debug.Log("Ouch!");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Package" && !hasPackage)
+        {
+            Debug.Log("Package has been collected");
+            hasPackage = true;
+            Destroy(other.gameObject,destroyDelay);
+        }
+
+        if (other.tag == "Customer" && hasPackage)
+        {
+            Debug.Log("Package has been delivered!");
+            hasPackage = false;
+        }
+    }
+}
