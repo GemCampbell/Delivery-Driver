3839774148dfe5c90f4517a28627539d18e05e49
Gemma Campbell <gem.cam89@outlook.com>
2024-11-23 17:12:47 +0000

    Added boosts to increase player speed, and added reduced speed when player collides with game objects

--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -6,20 +6,32 @@ using TMPro;
 
 public class Driver : MonoBehaviour
 {
-    public float moveSpeed;
+    //SPEEDS
+    public float currentSpeed;
+    public float normalSpeed;
+    public float slowSpeed;
+    public float boostSpeed;
     public float turnSpeed;
+
+    //TIMERS
+    public float slowSpeedTimer;
+    public float slowSpeedTimerMax;
+    public float boostTimer;
+    public float boostTimerMax;
+    public bool hasCollided;
 
     public bool hasPackage;
     public float destroyDelay;
 
     public int playerScore;
-    public int scoreAmount;
+    public int scoreValue;
 
     public TextMeshProUGUI scoreText;
-    
-    
+
+
     void Start()
     {
+        currentSpeed = normalSpeed; 
         hasPackage = false;
         playerScore = 0;
         UpdateScoreText();
@@ -29,16 +41,40 @@ public class Driver : MonoBehaviour
     void Update()
     {
         float turnAmount = Input.GetAxis("Horizontal") * turnSpeed * Time.deltaTime ;     //RETURNS A VALUE BETWEEN 1 AND -1 DEPENDING ON PLAYER INPUT
-        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * currentSpeed * Time.deltaTime;
 
         transform.Rotate(0, 0, -turnAmount);
 
         transform.Translate(0, moveAmount, 0);
+
+        if (currentSpeed == slowSpeed && hasCollided == true)
+        {
+            slowSpeedTimer += Time.deltaTime;
+            
+            if (slowSpeedTimer > slowSpeedTimerMax)
+            {
+                hasCollided = false;
+                currentSpeed = normalSpeed;
+                slowSpeedTimer = 0;
+            }
+        }
+
+
+        if (currentSpeed == boostSpeed)
+        {
+            boostTimer += Time.deltaTime;
+
+            if (boostTimer > boostTimerMax)
+            {
+                currentSpeed = normalSpeed;
+                boostTimer = 0;
+            }
+        }
     }
 
 
     void OnTriggerEnter2D(Collider2D other)
-    {
+    {        
         if (other.gameObject.tag == "Package" && hasPackage == false)
         {
             hasPackage = true;
@@ -48,11 +84,24 @@ public class Driver : MonoBehaviour
         
         if (other.gameObject.tag == "Customer" && hasPackage == true)
         {
-            playerScore += scoreAmount;
+            playerScore += scoreValue;
             UpdateScoreText();
 
             hasPackage = false;
         }
+
+
+        if (other.gameObject.tag == "Boost")
+        {
+            currentSpeed = boostSpeed;
+        }
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        currentSpeed = slowSpeed;
+        hasCollided = true;
     }
 
 
